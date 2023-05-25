02fed1938ffdc191a200b0b67f3a51a5b479ed74
Imtiaz-96 <97111908+Imtiaz-96@users.noreply.github.com>
2023-05-26 02:01:49 +0600

    initial

---
+++ hello_world.py
@@ -0,0 +1,72 @@
+A commercial indoor+outdoor thermometer is being sold as supporting LwM2M. The Object ID for a temperature object is 3303 
+and the Resource ID for the temperature value is 5700. The vendor has denoted Instance 0 for indoor temperature and 
+Instance 1 for outdoor temperature. If the device is reachable at thermometer.example.com what would be the URI for 
+obtaining outdoor temperature?
+Select one:
+
+1.
+coaps://thermometer.example.com/3303/5700/1
+2.
+http://thermometer.example.com/lwm2m/3303/1/5700/
+3.
+coap://thermometer.example.com:5683/3303/1/5700/
+4.
+lwm2m+coap://3303.thermometer.example.com?res=5700&ins=0
+
+Question 2
+Complete
+Mark 0.00 out of 0.50
+Flag question
+Question text
+
+In Sigfox
+Select one:
+
+a.base stations cannot initiate communication with the end devices
+
+b.message delivery is guaranteed by having all nearby base stations receive all messages sent from an end device
+
+c.end device classes are used to provide different Quality of Service options
+
+d.the synchronous communication supports low latency requirements
+
+Question 3
+Complete
+Mark 0.00 out of 0.50
+Flag question
+Question text
+
+You are designing IoT device to track voluntary cyclists, to find out what kind of routes are popular in the city and 
+its suburban areas. Your device will send regular updates of cyclist’s location and later you hope you can add some 
+other sensors to collect more detailed data throughout the route. Your device has very limited data storage capabilities.
+ During the process you must decide, what radio technology you will use for communication. From the following, which 
+ would be most suitable for your use case: 
+Select one:
+1.NB-IoT 
+2.Sigfox 
+3.ZigBee
+4.Z-Wave
+
+
+Question 4
+Complete
+Mark 0.50 out of 0.50
+Flag question
+Question text
+
+6LoWPAN devices can perform native IPv6 communication without any assistance with remote IPv6 hosts.
+Select one:
+
+1.True, however, it must be joined with CoAP on transport layer 
+
+2.True, 6LoWPAN compresses IPv6 header files to make the communication possible 
+
+3.False, 6LoWPAN border router is needed to facilitate communication between 6LoWPAN hosts and traditional IPv6 host 
+
+4.False, all constrained devices are not IPv6 capable, so IPv6 communication is impossible.
+
+Question 5
+Complete
+Mark 0.50 out of 0.50
+Flag question
+print ("Hello World!") 
